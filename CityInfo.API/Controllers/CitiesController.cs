9bc9764140ed85a4c71a968e1e9fb0cba1bf1473
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-01-21 16:38:04 +0100

    add CitiesController

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController] //Automatic HTTP 400 responses; Problem details for error status codes etc.
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object> {
+                    new { id = 1, Name = "New York City"},
+                    new { id = 2, Name = "Skopje"},
+                });
+        }
+    }
+}
